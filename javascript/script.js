62e343782170a6d7ba8e4c8285acda10d76bf73e
Wamaitha <wamaithaweru19@gmail.com>
2022-03-13 23:21:36 +0300

    add jquery simple effects

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,39 @@
-$(document).ready();
-$("#brand").hover(function () {
-  $("#delani").css("display","block");
-},
-function(){
-    $('#delani').css("display","none");
-}
-);
+$(document).ready(function(){
+  $('#brand').hover(function(){
+    $('#delani').css("display","block")
+  },
+    function(){
+      $('#delani').css("display","none")
+    });
+  });
+  
+  // mouse
 
+// $('#mouse-toggle').click(function(){
+//   $('#mouse').slideToggle("fast");
+// })
+
+$('#clickable1').click(function(){
+  $("#text1").show(function(){
+    $("#text1").hide(slow);
+  })
+  
+});
+
+$('#clickable2').click(function(){
+  $("#text2").show(function(){
+    $("#text2").hide(slow);
+  })
+  
+});
+
+$('#clickable3').click(function(){
+  $("#text3").show(function(){
+    $("#text3").hide(slow);
+  })
+  
+});
+
+// $('#brand').hover(function(){
+//   $('#mouse').slideup();
+//   $('#mouse').slidedown();
