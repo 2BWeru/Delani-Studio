d8a949b2e6a2e75bbdfe5d3c52ea378c41252cf2
Wamaitha <wamaithaweru19@gmail.com>
2022-03-14 02:36:00 +0300

    edit css styles

--- javascript/script.js
+++ javascript/script.js
@@ -7,33 +7,34 @@ $(document).ready(function(){
     });
   });
   
-  // mouse
-
-// $('#mouse-toggle').click(function(){
-//   $('#mouse').slideToggle("fast");
-// })
-
+// what we do
 $('#clickable1').click(function(){
   $("#text1").show(function(){
-    $("#text1").hide(slow);
-  })
+    $("#text1").hide(7000);
+  });
   
 });
 
 $('#clickable2').click(function(){
   $("#text2").show(function(){
-    $("#text2").hide(slow);
-  })
+    $("#text2").hide(7000);
+  });
   
 });
 
 $('#clickable3').click(function(){
   $("#text3").show(function(){
-    $("#text3").hide(slow);
-  })
+    $("#text3").hide(7000)
+    });
+  });
   
-});
+// projects
 
-// $('#brand').hover(function(){
-//   $('#mouse').slideup();
-//   $('#mouse').slidedown();
+
+$('#brand').hover(function(){
+  $('#delani').css("display","block")
+},
+  function(){
+    $('#delani').css("display","none")
+  });
+
