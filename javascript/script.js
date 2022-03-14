78bcaeba545a22200ca405ecf5af5a35c5bde6c6
Wamaitha <wamaithaweru19@gmail.com>
2022-03-14 13:19:19 +0300

    edit css styles

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,107 @@
-$(document).ready(function(){
-  $('#brand').hover(function(){
-    $('#delani').css("display","block")
-  },
-    function(){
-      $('#delani').css("display","none")
-    });
-  });
-  
+$(document).ready(function () {
+  $("#brand").hover(
+    function () {
+      $("#delani").css("display", "block");
+    },
+    function () {
+      $("#delani").css("display", "none");
+    }
+  );
+});
+
 // what we do
-$('#clickable1').click(function(){
-  $("#text1").show(function(){
-    $("#text1").hide(7000);
+$("#clickable1").click(function () {
+  $("#text1").show(function () {
+     $("#text1").hide(7000);
   });
-  
 });
 
-$('#clickable2').click(function(){
-  $("#text2").show(function(){
-    $("#text2").hide(7000);
+$("#clickable2").click(function () {
+  $("#text2").show(function () {
+     $("#text2").hide();
   });
-  
 });
 
-$('#clickable3').click(function(){
-  $("#text3").show(function(){
-    $("#text3").hide(7000)
-    });
+$("#clickable3").click(function () {
+  $("#text3").show(function () {
+     $("#text3").hide(7000);
   });
-  
+});
+
 // projects
+$(".proj-1").hover(
+  function () {
+    $(".name1-overlay").css("display", "block");
+  },
+  function () {
+    $(".name1-overlay").css("display", "none");
+  }
+);
+
+$(".proj-2").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-3").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-4").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-5").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-6").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-7").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
+$(".proj-8").hover(
+  function () {
+    $(".name-overlay").css("display", "block");
+  },
+  function () {
+    $(".name-overlay").css("display", "none");
+  }
+);
 
 
-$('#brand').hover(function(){
-  $('#delani').css("display","block")
-},
-  function(){
-    $('#delani').css("display","none")
-  });
 
+$("#brand").hover(
+  function () {
+    $("#delani").css("display", "block");
+  },
+  function () {
+    $("#delani").css("display", "none");
+  }
+);
